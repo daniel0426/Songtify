24b460c944e1e324116f841669b151c50acf62c3
Daniel <angus9209@gmail.com>
2022-10-11 22:38:47 +1300

    Implement artist details page

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,5 +1,36 @@
-const ArtistDetails = () => (
-  <div>ArtistDetails</div>
-);
+/* eslint-disable no-unused-vars */
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+
+const ArtistDetails = () => {
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const {
+    data: artistData,
+    isFetching: isFetchingArtistDetails,
+    error,
+  } = useGetArtistDetailsQuery(artistId);
+
+  if (isFetchingArtistDetails)
+    return <Loader title="Searching artist details" />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col ml-6">
+      <DetailsHeader artistId={artistId} artistData={artistData} />
+
+      <RelatedSongs
+        data={Object.values(artistData?.songs)}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
+};
 
 export default ArtistDetails;
