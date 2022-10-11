50cb23ce023b7df235c9a6b6f50f73d4194e9fa5
Daniel <angus9209@gmail.com>
2022-10-11 23:10:26 +1300

    complete top-charts & top-artists page

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -53,7 +53,10 @@ const TopPlay = () => {
   };
 
   return (
-    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+    >
       <div className="w-full flex flex-col">
         <div className="flex flex-row justify-between items-center">
           <h2 className="text-white font-bold">Top Charts</h2>
@@ -63,22 +66,45 @@ const TopPlay = () => {
         </div>
         <div className="mt-4 flex flex-col gap-1">
           {topPlays?.map((song, i) => (
-            <TopChartCard key={song.key} song={song} i={i} isPlaying={isPlaying} activeSong={activeSong} handlePauseClick={() => handlePauseClick(song, i)} handlePlayClick={() => handlePlayClick(song, i)} />
+            <TopChartCard
+              key={song.key}
+              song={song}
+              i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={() => handlePauseClick(song, i)}
+              handlePlayClick={() => handlePlayClick(song, i)}
+            />
           ))}
         </div>
       </div>
       <div>
         <div className="flex flex-row justify-between items-center">
           <h2 className="text-white font-bold">Top Artists</h2>
-          <Link to="/top-charts">
+          <Link to="/top-artists">
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
         </div>
-        <Swiper slidesPerView="auto" spaceBetween={15} centeredSlides centeredSlidesBounds modules={[FreeMode]} className="mt-4">
+        <Swiper
+          slidesPerView="auto"
+          spaceBetween={15}
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4"
+        >
           {topPlays?.map((song, i) => (
-            <SwiperSlide key={song?.key} style={{ width: '25%', height: 'auto' }} className="shadow-lg rounded-full animate-slideright">
+            <SwiperSlide
+              key={song?.key}
+              style={{ width: '25%', height: 'auto' }}
+              className="shadow-lg rounded-full animate-slideright"
+            >
               <Link to={`/artists/${song?.artists[0].adamid}`}>
-                <img src={song?.images.background} alt="name" className="rounded-full w-full object-cover" />
+                <img
+                  src={song?.images.background}
+                  alt="name"
+                  className="rounded-full w-full object-cover"
+                />
               </Link>
             </SwiperSlide>
           ))}
